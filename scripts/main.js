036e0f4121b972aa24112b5a590fd087bbb6e212
stra <str.tnk18@icloud.com>
2026-07-26 12:40:51 +0900

    Initialize Phaser game with multiple scenes

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+import { 
+    BootScene, 
+    TitleScene, 
+    MainMenuScene, 
+    OptionScene, 
+    BattleSelectScene, 
+    PlayScene, 
+    EditorScene 
+} from "./scenes.js";
+
+window.addEventListener("DOMContentLoaded", async () => {
+    await document.fonts.load('16px "Determination"');
+    await document.fonts.load('16px "JF東雲ゴシック"');
+    await document.fonts.ready;
+
+    const config = {
+        type: Phaser.WEBGL,
+        width: 640,
+        height: 480,
+        parent: "game",
+        backgroundColor: "#000000",
+
+        pixelArt: true,
+
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH
+        },
+
+        physics: {
+            default: "matter",
+            matter: {
+                gravity: { y: 0 },
+                debug: false
+            }
+        },
+
+        scene: [
+            BootScene,
+            TitleScene,
+            MainMenuScene,
+            OptionScene,
+            BattleSelectScene,
+            PlayScene,
+            EditorScene
+               ]
+    };
+
+    new Phaser.Game(config);
+});
