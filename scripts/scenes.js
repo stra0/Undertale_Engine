4134a2f87be9fb96f8c53f819c7521eeac5ba08f
stra <str.tnk18@icloud.com>
2026-08-21 07:21:20 +0900

    debug

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -975,11 +975,9 @@ export class BattleChoiceScene extends UndertaleScene {
 
     init(data) {
         this.files = data.assets;
-        alert("init")
     }
 
     preload() {
-        alert("preload")
         if (!this.files) return;
         for (const [key,assets] of this.files) {
             const isBitmap = assets.some(asset => asset.type ==="bitmap");
@@ -994,11 +992,9 @@ export class BattleChoiceScene extends UndertaleScene {
                 }
             }
         }
-        alert("finishpre")
     }
 
     onCreate() {
-        alert("oncreate")
         if (this.files) {
             for (const assets of this.files.values()) {
                 for (const asset of assets) {
@@ -1009,13 +1005,9 @@ export class BattleChoiceScene extends UndertaleScene {
             }
         }
 
-        alert("bcs");
-
         this.items = [];
 
-        const battles = this.cache.json.get("battleData").battles;
-
-        alert(battles);
+        const battles = this.cache.json.get("data/battleData").battles;
 
         alert(Object.keys(battles)[0]);
 
